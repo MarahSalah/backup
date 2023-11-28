8d99054ebbc1e18dcc06a5fc56b6a1294ac392bd
Marah Salah <marahsalah9@gmail.com>
2023-11-28 12:43:43 +0300

    mm

---
+++ client/client/src/pages/CartContext.jsx
@@ -0,0 +1,41 @@
+// CartContext.js
+import React, { createContext, useReducer, useContext } from 'react';
+
+const CartContext = createContext();
+
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TO_CART':
+      return {
+        ...state,
+        items: [...state.items, action.payload],
+      };
+    // You can add other cases as needed, like REMOVE_FROM_CART, CLEAR_CART, etc.
+    default:
+      return state;
+  }
+};
+
+const CartProvider = ({ children }) => {
+  const [cartState, dispatch] = useReducer(cartReducer, { items: [] });
+
+  const addToCart = (item) => {
+    dispatch({ type: 'ADD_TO_CART', payload: item });
+  };
+
+  return (
+    <CartContext.Provider value={{ cartState, addToCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+const useCart = () => {
+  const context = useContext(CartContext);
+  if (!context) {
+    throw new Error('useCart must be used within a CartProvider');
+  }
+  return context;
+};
+
+export { CartProvider, useCart };
